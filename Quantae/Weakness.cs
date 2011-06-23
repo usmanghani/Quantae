9d6660727ebb473ed88769c0914fdb5ef008f436
unknown <Owner@.(none)>
2011-06-22 17:30:35 -0700

    Update WeaknessType and Weakness filenames

--- Quantae/Weakness.cs
+++ Quantae/Weakness.cs
@@ -5,17 +5,8 @@ using System.Text;
 
 namespace Quantae
 {
-    public enum WeaknessType
+    public class Weakness
     {
-        Unknown = 0,
-
-        // Minor weakness
-        NumberAgreement = 1,
-        GenderAgreement = 2,
-
-        // Major weakness.
-        Understanding = 3,
-
-        UmbrellaTopic = 4,
+        
     }
 }
