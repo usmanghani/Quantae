5f88a29fb810e02e1c365c4da04c69e2855c6d37
Usman <usman.ghani@gmail.com>
2011-10-26 21:31:57 -0700

    test mvc app to test mvc features

---
+++ TestMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult jsontest()
+        {
+            TestMvcApp.Models.TestModel model = new Models.TestModelDerived();
+            model.ContextualAnalysis.Add(new Models.ContextualAnalysis { startIndex = new int[] { 5 }, endIndex = new int[] { 4 } });
+            model.GrammarEntries.Add(new Models.GrammarEntry { Word = "word", Translation = "translation" });
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TestMvcApp/Models/TestModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMvcApp.Models
+{
+    public struct ContextualAnalysis
+    {
+        public int[] startIndex { get; set; }
+        public int[] endIndex { get; set; }
+    }
+
+    public struct GrammarEntry
+    {
+        public string Word { get; set; }
+        public string Translation { get; set; }
+    }
+
+    public class TestModel
+    {
+        public List<ContextualAnalysis> ContextualAnalysis { get; set; }
+        public List<GrammarEntry> GrammarEntries { get; set; }
+
+        public TestModel()
+        {
+            ContextualAnalysis = new List<ContextualAnalysis>();
+            GrammarEntries = new List<GrammarEntry>();
+        }
+    }
+
+    public class TestModelDerived : TestModel
+    {
+        public int Index { get; set; }
+    }
+}
