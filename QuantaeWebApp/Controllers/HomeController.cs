74b2e8bda06039f4fa174ef626f8af9fc4c05d92
Usman Ghani <usman.ghani@gmail.com>
2012-04-24 22:14:49 -0700

    yaaayyy got logging working now finally after a long time seriously dood what have u been doing for so long this is a really long commit comment that is really going to be amended later on because u know just cz git lets us do that

--- QuantaeWebApp/Controllers/HomeController.cs
+++ QuantaeWebApp/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace QuantaeWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(HomeController).Name);
         public ActionResult Index()
         {
+            log.Info("this is awesome");
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                log.Info(string.Format("really sire {0}", i));
+            }
             return View();
         }
 
--- QuantaeWebApp/Controllers/ReviewController.cs
+++ QuantaeWebApp/Controllers/ReviewController.cs
@@ -7,6 +7,7 @@ using Quantae.ViewModels;
 using Quantae.DataModel;
 using Quantae.Engine;
 using System.Diagnostics;
+using System.Net;
 
 namespace QuantaeWebApp.Controllers
 {
@@ -24,9 +25,14 @@ namespace QuantaeWebApp.Controllers
 
             GetNextSentenceResult result = engine.GetNextSentence(profile);
 
+            if (result == null)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, "No donut for you!");
+            }
             if (!result.IsReview)
             {
                 Trace.TraceError("Unexpected sentence result.  Should only receive Review sentences during review flow.");
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, "You are not supposed to be here!");
             }
             
             ReviewViewModel model = new ReviewViewModel(result.Sentence.SentenceText, result.Sentence.SentenceTranslation);
